1273e6479070c3f49f7d671abc5ba13eb23889c3
SuneetaGang <suneeta.gangwar@gmail.com>
2019-06-18 02:05:49 +0530

    program to parse file

---
+++ src/main/java/files/FileParser.java
@@ -0,0 +1,49 @@
+package files;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+
+public class FileParser {
+
+	public static void main(String[] args) throws IOException {
+		fileReader();
+
+	}
+
+	public static void fileReader() throws IOException {
+
+		// todo get file location
+		// todo open file stream
+		// todo start iterating over file content
+		// todo processing over data
+		// todo close file stream
+		String text = "Suneeta";
+
+		String inputFile = "E:\\Testing\\test1.txt";
+		String outputFile = "E:\\Testing\\test2.txt";
+		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+		String lines;
+		while ((lines = reader.readLine()) != null) {
+
+			if (lines.contains(text)) {
+				writer.write(lines);
+				writer.newLine();
+
+			}
+
+		}
+		writer.flush();
+		writer.close();
+		reader.close();
+
+	}
+
+}
